5fe8afdef327dd5ec5e2a0f2cdadb2b2ddd05ed7
joseph <1396871335@qq.com>
2020-10-27 19:06:21 +0800

    修改前端注册页面，绑定时间

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+
+
+class RegisterView(View):
+    """用户注册"""
+
+    def get(self, request):
+        # 提供用户注册页面
+        return render(request, 'register.html')
+
+    def psot(self,request):
+        # 实现用户注册逻辑
+        # 接收参数
+        value = request.POST.get('key')
+        pass
+
+        # 校验参数
+        # 保存注册数据
+        # 响应结果
--- meiduo_mall/meiduo_mall/static/js/register.js
+++ meiduo_mall/meiduo_mall/static/js/register.js
@@ -1,207 +1,97 @@
-var vm = new Vue({
-	el: '#app',
-	data: {
-		host,
-		error_name: false,
-		error_password: false,
-		error_check_password: false,
-		error_phone: false,
-		error_allow: false,
-		error_sms_code: false,
+// 创建vue对象
+// 采用的是es6语法
+let vm = new Vue({
+    el:'#app',  //
+    delimiters:['[[',']]'],
+    data:{// 数据对象
+        //v-model
+        username:'',
+        password:'',
+        password2:'',
+        mobile:'',
+        image_code:'',
+        sms_code:'',
+        allow:'',
 
-		username: '',
-		password: '',
-		password2: '',
-		mobile: '', 
-		sms_code: '',
-		allow: false,
+        // v-show
+        error_name : false,
+        error_password:false,
+        error_password2:false,
+        error_mobile: false,
+        error_image_code:false,
+        error_sms_code:false,
+        error_allow:false,
 
-		sms_code_tip: '获取短信验证码',
-        sending_flag: false, // 正在发送短信标志
-		error_name_message: '', // 用户名错误提示
-		error_phone_message: '', // 手机号错误提示
-        error_sms_code_message: '' // 短信验证码错误
-	},
-	methods: {
-		// 检查用户名
-		check_username: function (){
-            var len = this.username.length;
-            if(len<5||len>20) {
-                this.error_name_message = '请输入5-20个字符的用户名';
+        // error_massage
+        error_name_message:'',
+        error_mobile_message:'',
+
+    },
+    methods:{// 定义和实现事件方法
+        // 校验用户名
+        check_username(){
+            // 用户名5-20个字符[a-zA-Z0-9_-]
+            let re = /^[a-zA-Z0-9_-]{5,20}$/;
+            if(re.test(this.username)){
+                // 匹配成功，不展示错误信息
+                this.erroe_name = false;
+            }else {
+                //匹配失败，展示错误信息
+                this.error_name_message = '请输入5-20个字符的用户名'
                 this.error_name = true;
-            } else {
-                this.error_name = false;
-            }
-            // 检查重名
-            if (this.error_name == false) {
-                axios.get(this.host + '/usernames/' + this.username + '/count/', {
-                        responseType: 'json'
-                    })
-                    .then(response => {
-                        if (response.data.count > 0) {
-                            this.error_name_message = '用户名已存在';
-                            this.error_name = true;
-                        } else {
-                            this.error_name = false;
-                        }
-                    })
-                    .catch(error => {
-                        console.log(error.response.data);
-                    })
             }
         },
-		check_pwd: function (){
-			var len = this.password.length;
-			if(len<8||len>20){
-				this.error_password = true;
-			} else {
-				this.error_password = false;
-			}		
-		},
-		check_cpwd: function (){
-			if(this.password!=this.password2) {
-				this.error_check_password = true;
-			} else {
-				this.error_check_password = false;
-			}		
-		},
-		// 检查手机号
-		check_phone: function (){
-            var re = /^1[345789]\d{9}$/;
-            if(re.test(this.mobile)) {
-                this.error_phone = false;
-            } else {
-                this.error_phone_message = '您输入的手机号格式不正确';
-                this.error_phone = true;
-            }
-            if (this.error_phone == false) {
-                axios.get(this.host + '/mobiles/'+ this.mobile + '/count/', {
-                        responseType: 'json'
-                    })
-                    .then(response => {
-                        if (response.data.count > 0) {
-                            this.error_phone_message = '手机号已存在';
-                            this.error_phone = true;
-                        } else {
-                            this.error_phone = false;
-                        }
-                    })
-                    .catch(error => {
-                        console.log(error.response.data);
-                    })
+        // 校验密码
+        check_pwd(){
+            let re = /^[0-9A-Za-z]{8,20}$/;
+            if (re.test(this.password)){
+                this.error_password = false
+            }else {
+                this.error_password=true
             }
         },
-		check_sms_code: function(){
-			if(!this.sms_code){
-				this.error_sms_code = true;
-			} else {
-				this.error_sms_code = false;
-			}
-		},
-		check_allow: function(){
-			if(!this.allow) {
-				this.error_allow = true;
-			} else {
-				this.error_allow = false;
-			}
-		},
-		// 发送手机短信验证码
-        send_sms_code: function(){
-
-		    // 重新发送短信后，隐藏提示信息
-		    this.error_sms_code = false;
-
-            if (this.sending_flag == true) {
-                return;
+        // 校验两次输入的密码是否一致
+        check_pwd2(){
+            if (this.password === this.password2){
+                this.error_password2 = false
+            }else {
+                this.error_password2 = true
             }
-            this.sending_flag = true;
-
-            // 校验参数，保证输入框有数据填写
-            this.check_phone();
-
-            if (this.error_phone == true) {
-                this.sending_flag = false;
-                return;
-            }
-
-            // 向后端接口发送请求，让后端发送短信验证码
-            axios.get(this.host + '/sms_codes/' + this.mobile + '/', {
-                    responseType: 'json'
-                })
-                .then(response => {
-                    // 表示后端发送短信成功
-                    // 倒计时60秒，60秒后允许用户再次点击发送短信验证码的按钮
-                    var num = 60;
-                    // 设置一个计时器
-                    var t = setInterval(() => {
-                        if (num == 1) {
-                            // 如果计时器到最后, 清除计时器对象
-                            clearInterval(t);
-                            // 将点击获取验证码的按钮展示的文本回复成原始文本
-                            this.sms_code_tip = '获取短信验证码';
-                            // 将点击按钮的onclick事件函数恢复回去
-                            this.sending_flag = false;
-                        } else {
-                            num -= 1;
-                            // 展示倒计时信息
-                            this.sms_code_tip = num + '秒';
-                        }
-                    }, 1000, 60)
-                })
-                .catch(error => {
-                    if (error.response.status == 400) {
-                        // 展示发送短信错误提示
-                        this.error_sms_code = true;
-                        this.error_sms_code_message = error.response.data.message;
-                    } else {
-                        console.log(error.response.data);
-                    }
-                    this.sending_flag = false;
-                })
         },
-		// 注册
-        on_submit: function(){
+        // 校验手机号是否正确
+        check_mobile(){
+            let re =/^1[0-9]{10}$/;
+            if (re.test(this.mobile)){
+                this.error_mobile = false
+            }else {
+                this.error_mobile_message = '请输入正确的手机号'
+                this.error_mobile = true
+            }
+        },
+        check_image_code(){},
+        check_sms_code(){},
+        check_allow(){
+            if (!this.allow){
+                this.error_allow = true
+            }else {
+                this.error_allow =false;
+            }
+        },
+        // 监听表单提交事件
+        on_submit(){
             this.check_username();
             this.check_pwd();
-            this.check_cpwd();
-            this.check_phone();
-            this.check_sms_code();
+            this.check_pwd2();
+            this.check_mobile();
             this.check_allow();
-
-            if(this.error_name == false && this.error_password == false && this.error_check_password == false
-                && this.error_phone == false && this.error_sms_code == false && this.error_allow == false) {
-                axios.post(this.host + '/users/', {
-                        username: this.username,
-                        password: this.password,
-                        password2: this.password2,
-                        mobile: this.mobile,
-                        sms_code: this.sms_code,
-                        allow: this.allow.toString()
-                    }, {
-                        responseType: 'json'
-                    })
-                    .then(response => {
-                        // 记录用户的登录状态
-                        sessionStorage.clear();
-                        localStorage.clear();
-                        localStorage.token = response.data.token;
-                        localStorage.username = response.data.username;
-                        localStorage.user_id = response.data.user_id;
-                        location.href = '/login.html';
-                    })
-                    .catch(error=> {
-                        if (error.response.status == 400) {
-                            if ('non_field_errors' in error.response.data) {
-                                this.error_sms_code_message = error.response.data.non_field_errors[0];
-                            } else {
-                                this.error_sms_code_message = '数据有误';
-                            }
-                            this.error_sms_code = true;
-                        } else {
-                            console.log(error.response.data);
-                        }
-                    })
+            if (this.erroe_name == true || this.error_password == true||this.error_password2==true||this.error_allow==true||this.error_mobile==true){
+                // window.event.returnValue =false;
+                window.event.preventDefault();
+                // return false
             }
-        }
-	}
+
+        },
+        generate_image_code(){},
+        send_sms_code(){},
+    }
 });
